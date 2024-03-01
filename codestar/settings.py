b65ead0b7ad57d96f7b4f8c6793c007052600a52
Sarolta <sarolta.hegyi@gmail.com>
2024-03-01 16:43:17 +0100

    add heroku to allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=%+jgs*f&sfplf-o*&1jyestp2b$-vbtfe*om1s8xsbal98q(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ".herokuapp.com"]
 
 
 # Application definition
