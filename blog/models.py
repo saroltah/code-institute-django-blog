ae5e15b5b939840fcfe0e9a27c613a56d43bab8d
Sarolta <sarolta.hegyi@gmail.com>
2024-03-11 17:43:39 +0100

    add meta class and def str to comment and post

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f" {self.title} written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -26,5 +30,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(default=timezone.now)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f" comment {self.body} by {self.author}"
 
    
