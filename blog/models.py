9d3e211b84846d8338bad941329a289bfac87756
Sarolta <sarolta.hegyi@gmail.com>
2024-03-11 08:47:46 +0100

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -18,4 +18,11 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-  content = models.TextField()
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+   
